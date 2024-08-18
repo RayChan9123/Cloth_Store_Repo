115fae0eb8764924964e6d7887aaa7302feecbd4
Ray C <raychanray64@gmail.com>
2024-08-18 11:12:53 -0400

    "Added CORS middleware to backend, updated package-lock.json and package.json accordingly"

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,26 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Cloth } from "./models/clothModel.js";
 import clothesRoute from './routes/clothesRoute.js'
+import cors from 'cors'
 
 const app = express();
 
 //Middleware for paring request body
 app.use(express.json());
+
+//Middleware for handling CORS POLICY
+// Opt1: Allow All origins with default of cors(*)
+app.use(cors())
+// Opt2: Allow custom Origins
+app.use(
+    cors({
+        origin: 'http://localhost:4000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type']
+    })
+);
+
+
 
 app.get('/', (req, res) => {
     console.log(req);
