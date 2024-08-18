072f559a683fa45b0dfe8430f3c5ab8f2adf69ef
Ray C <raychanray64@gmail.com>
2024-08-17 22:55:09 -0400

    "Added config.js to .gitignore and updated backend/package.json with dependencies"

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 4000
+
+export const mongoDBURL = 'mongodb+srv://raychan:ceonxD7rb1Xab2yM@cluster0.qbi3kki.mongodb.net/clothes-collection?retryWrites=true&w=majority&appName=Cluster0';
