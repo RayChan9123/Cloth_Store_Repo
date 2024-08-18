4229e388e57edddfdc388c713b9a35a8960fd3ab
Ray C. <raychanray64@gmail.com>
2024-08-17 22:55:51 -0400

    Delete backend/config.js

--- backend/config.js
+++
@@ -1,3 +0,0 @@
-export const PORT = 4000
-
-export const mongoDBURL = 'mongodb+srv://raychan:ceonxD7rb1Xab2yM@cluster0.qbi3kki.mongodb.net/clothes-collection?retryWrites=true&w=majority&appName=Cluster0';
