a7607ec6dc9d1774d10362a581a0e92efe12529d
Ray C <raychanray64@gmail.com>
2024-08-18 12:29:11 -0400

    "Added react-router-dom dependency and implemented routing in App component"

--- Cloth Store Frontend/src/App.jsx
+++ Cloth Store Frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ShowCloth from './pages/ShowCloth';
+import EditCloth from './pages/EditCloth';
+import DeleteCloth from './pages/DeleteCloth';
+import CreateClothes from './pages/CreateClothes';
 
 const App = () => {
   return (
-    <div className='bg-blue-200 text-white'>App</div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/clothes/create" element={<CreateClothes />} />
+      <Route path="/clothes" element={<ShowCloth />} />
+      <Route path="/clothes/:id" element={<EditCloth />} />
+      <Route path="/clothes/delete/:id" element={<DeleteCloth />} />
+
+
+
+
+    </Routes>
   )
 }
 
