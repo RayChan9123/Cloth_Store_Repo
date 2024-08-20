0fd9b5e9ca146bdd52a89d0d090dfd7c507210cf
Ray Hiu Tsun Chan <rchan@cpos.com>
2024-08-20 10:20:18 -0400

    "Updated Spinner component and implemented cloth details page with API call and loading state"

--- Cloth Store Frontend/src/components/Spinner.jsx
+++ Cloth Store Frontend/src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Spinner = () => {
     return (
-        <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-500'>Spinner</div>
+        <div className='animate-ping w-32 h-32 m-8 rounded-full bg-sky-500'>Spinner</div>
     )
 }
 
