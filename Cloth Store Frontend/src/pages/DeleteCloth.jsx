8f687405e6f7af37d8e06409630bb1af7f116438
Ray Hiu Tsun Chan <rchan@cpos.com>
2024-08-20 21:10:50 -0400

    "Added functionality to DeleteCloth component, including state for loading, API call to delete cloth, and UI elements for confirmation and deletion"

--- Cloth Store Frontend/src/pages/DeleteCloth.jsx
+++ Cloth Store Frontend/src/pages/DeleteCloth.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom';
 
 const DeleteCloth = () => {
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const { id } = useParams();
+
+
+    const handleDeleteCloth = () => {
+        setLoading(true);
+        axios
+            .delete(`http://localhost:4000/clothes/${id}`)
+            .then(() => {
+                setLoading(false);
+                alert('Cloth Deleted successfully', { variant: 'success' });
+                navigate('/');
+            })
+            .catch((error) => {
+                setLoading(false);
+                alert('An error happened. Please Check console');
+                console.log(error);
+            });
+    };
+
     return (
-        <div>DeleteCloth</div>
+        <div className='p-4'>
+            <BackButton />
+            <h1 className='text-3xl my-4'>Delete Cloth</h1>
+            {loading ? <Spinner /> : ''}
+            <div className='flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto'>
+                <h3 className='text-2xl'>Are You Sure You want to delete this Cloth?</h3>
+
+                <button
+                    className='p-4 bg-red-600 text-white m-8 w-full'
+                    onClick={handleDeleteCloth}
+                >
+                    Yes, Delete it
+                </button>
+            </div>
+        </div>
     )
 }
 
-export default DeleteCloth
+export default DeleteCloth;
