806b7ef702d53b02cff29a4fa35c918fbedcbe03
Ray Hiu Tsun Chan <rchan@cpos.com>
2024-08-21 00:16:03 -0400

    "Removed 'config.js' from .gitignore, added comment to beginning of file"

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+// config.js
+
+export const PORT = process.env.PORT || 10000; // Uses environment variable for PORT, with fallback to 4000
+export const mongoDBURL = process.env.MONGODB_URL; // MongoDB URL set as an environment variable
